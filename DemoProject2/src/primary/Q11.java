a57d4d677ff2bb011e722e29920e588f74ace6e1
cassidyjclayton <43220444+cassidyjclayton@users.noreply.github.com>
2021-11-08 14:44:42 -0500

    Progress on Q11

---
+++ DemoProject2/src/primary/Q11.java
@@ -0,0 +1,11 @@
+package primary;
+  /*
+   Write a program that would access two float-variables from a class that
+   exists in another package. Note, you will need to create two packages
+   to demonstrate the solution.
+   */
+public class Q11{
+  public static void main(String[] args) {
+
+  }
+}

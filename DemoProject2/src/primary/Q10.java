5011ee20aa24f339e68f48c03e71e9011b40358b
cassidyjclayton <43220444+cassidyjclayton@users.noreply.github.com>
2021-11-09 17:39:07 -0500

    Finish 10

--- DemoProject2/src/primary/Q10.java
+++ DemoProject2/src/primary/Q10.java
@@ -5,20 +5,9 @@ Find the minimum of two numbers using ternary operators.
 import java.io*;
 
 public class Q10{
-	public static void main(String[] args) {
-		Minimum(int i, int j, int k) {
-			//declare variables in
-			i, j, min;
-
-			System.out.println("First number: " + i);
-			System.out.println("Second number: " + j);
-
-			// find larger # via ternary operators
-			min = (i < j) ? i : j;
-
-			// print largest #
-			System.out.println("The minimum # is: " + min);
-
-		}
+	public void Compare (int a, int b) {
+		String result = (a >= b)? a + " is greater than " + b : b + " is greater than " + a;
+		System.out.println("Which number is bigger? " + result);
 	}
 }
+// Code from Merve Kirali
